e2f3b50a32b4ff5bcd7fe06e81ebc0af892568c4
Sadanand012 <sadamare8408@gmail.com>
2023-01-10 23:48:14 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
-// <!-- typed js effect starts -->
-// var typed = new Typed(".typing-text", {
-//     strings: ["Backend Developer"],
-//     loop: true,
-//     typeSpeed: 50,
-//     backSpeed: 25,
-//     backDelay: 500,
-// });
-// <!-- typed js effect ends -->
 
-// window.onload = function(){
-//     GitHubCalendar(".calendar", "Sadanand012");
-
-//     // or enable responsive functionality:
-//     GitHubCalendar(".calendar", "Sadanand012", { responsive: true });
-
-//     // Use a proxy
-//     GitHubCalendar(".calendar", "Sadanand012", {
-//        proxy (Sadanand012) {
-//          return fetch(`https://your-proxy.com/github?user=Sadanand012`)
-//        }
-//     }).then(r => r.text())
-// }
-
-function toggleMobileMenu(menu){
+function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
-} 
+}
 
 async function fetchData(type = "skills") {
   let response
   type === "skills" ?
-      response = await fetch("./json file/skills.json")
-      :
-      response = await fetch("./json file/projects.json")
+    response = await fetch("./json file/skills.json")
+    :
+    response = await fetch("./json file/projects.json")
   const data = await response.json();
   return data;
 }
@@ -40,7 +17,7 @@ function showSkills(skills) {
   let skillsContainer = document.getElementById("skillsContainer");
   let skillHTML = "";
   skills.forEach(skill => {
-      skillHTML += `
+    skillHTML += `
       <div class="bar">
             <div class="info">
               <img src=${skill.icon} alt="skill" />
@@ -52,5 +29,5 @@ function showSkills(skills) {
 }
 
 fetchData().then(data => {
-    showSkills(data);
+  showSkills(data);
 });
