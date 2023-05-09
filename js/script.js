9c8328d9ba532b04e5b707f555bcda2120a693b2
Sadanand012 <sadamare8408@gmail.com>
2023-05-09 17:25:36 +0530

    resume

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ document.getElementById("resume-button-1").onclick = function() {newWindowResum(
 function newWindowResum(){
   window.open("https://drive.google.com/file/d/1rhRaGS7Vkb3qgADXcgosa_y9S5f4eOKh/view");
 }
-document.getElementById("resume-button-2").onclick = function() {newWindowResum()};
+document.getElementById("resume-link-2").onclick = function() {newWindowResum()};
 
 function newWindowResum(){
   window.open("https://drive.google.com/file/d/1rhRaGS7Vkb3qgADXcgosa_y9S5f4eOKh/view");
