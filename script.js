9297fa44fe8775a8e04ab21a525b4c6a0376c1bc
Sadanand012 <sadamare8408@gmail.com>
2023-10-28 10:57:06 +0530

    hide data from back end and client

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ hamburger.addEventListener('click', () => {
 
     menulist.classList.toggle('show');
 });
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+});
