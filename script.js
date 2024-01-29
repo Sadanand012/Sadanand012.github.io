a2f882a8eb65e76aef29ffa8caabe93d9ff55d58
Sadanand012 <sadamare8408@gmail.com>
2024-01-29 17:56:43 +0530

    changes

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ hamburger.addEventListener('click', () => {
 
     menulist.classList.toggle('show');
 });
-document.addEventListener("keydown", function (event){
-    if (event.ctrlKey){
-       event.preventDefault();
-    }
-    if(event.keyCode == 123){
-       event.preventDefault();
-    }
-});
+// document.addEventListener("keydown", function (event){
+//     if (event.ctrlKey){
+//        event.preventDefault();
+//     }
+//     if(event.keyCode == 123){
+//        event.preventDefault();
+//     }
+// });
